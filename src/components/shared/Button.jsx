47721b5d769f1f28db2984d6423af9b6c6a5e1a5
aasthan1206 <aastha.nayak2002@gmail.com>
2023-01-11 08:57:02 +0530

    update button

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({classname, text}) => {
   return (
     <div>
-      <button className={`px-[18px] py-[18px] font-extrabold rounded-[10px] transition-colors ease-in-out duration-300 overflow-visible ${classname}`}>{text}Button</button>
+      <button className={`px-[18px] py-[18px] font-extrabold rounded-[10px] transition-colors ease-in-out duration-300 overflow-visible ${classname}`}>{text}</button>
     </div>
   )
 }
